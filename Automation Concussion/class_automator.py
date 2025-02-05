ec9c74749c55e09bf12d0b9a651e8f61b10120ac
Megan Tran <68253811+MeganTran6023@users.noreply.github.com>
2025-02-05 03:53:12 -0500

    Add files via upload

--- Automation Concussion/class_automator.py
+++ Automation Concussion/class_automator.py
@@ -3,6 +3,7 @@
 #Get count of num of patients in each bucket 
 
 import pandas as pd
+import time
 
 class ConcussionAutomator:
     
@@ -48,15 +49,33 @@ class ConcussionAutomator:
         
         
     #Get average days 2 clearance for each bucket
+    def avg_buckets_d2c(self):
+        avg_bucket1 = self.df.loc[self.df['buckets'] == 1, 'days_2_clearance'].mean() #in bucket = 1 find average of days2clearance
+        avg_bucket2 = self.df.loc[self.df['buckets'] == 2, 'days_2_clearance'].mean()
+        print(f"Average Days to Clearance for patients recovered under a month: {round(avg_bucket1,2)}\n")
+        print(f"Average Days to Clearance for patients recovered exactly one month and over: {round(avg_bucket2,2)}\n")
 
-        
+    #output results
     def run(self):
+        start_time = time.time()
+        end_time = time.time() 
+        elapsed_time = end_time - start_time
+        
+        start_time_cpu = time.process_time() # CPU time
+        end_time_cpu = time.process_time() # End of CPU time
+        elapsed_time_cpu_ms = (end_time_cpu - start_time_cpu) * 1000
+        
         print("Concusion Patient Dataset Analyzer")
         print("---------------------------------------")
         self.clean_data()
         self.bucket_count()
         print("---------------------------------------")
-
-
-
+        self.avg_buckets_d2c()
+        elapsed_time_ms = (end_time - start_time) * 1000  # Calculate in milliseconds
 #add time taken to run program
+        print(f"\nTime taken for script: {elapsed_time_ms:.6f} milliseconds")  # More decimal places
+        print(f"CPU time taken for script: {elapsed_time_cpu_ms:.6f} milliseconds")
+
+
+
+
